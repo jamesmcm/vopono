1c8b6ff0094808569253aa570f3dca655322e291
Jeff Hughes <jeff.hughes@gmail.com>
2021-02-08 18:46:48 -0500

    Add NordVPN as provider

---
+++ src/providers/nordvpn/mod.rs
@@ -0,0 +1,16 @@
+mod openvpn;
+
+use super::{ConfigurationChoice, OpenVpnProvider, Provider};
+use crate::vpn::Protocol;
+
+pub struct NordVPN {}
+
+impl Provider for NordVPN {
+    fn alias(&self) -> String {
+        "nordvpn".to_string()
+    }
+
+    fn default_protocol(&self) -> Protocol {
+        Protocol::OpenVpn
+    }
+}
