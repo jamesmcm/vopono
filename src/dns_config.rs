eb6ac8546e956e9c0d531499c4159779b5604e83
arch <arch@local>
2022-04-06 19:07:46 +0200

    generate nsswitch.conf only if exist

--- src/dns_config.rs
+++ src/dns_config.rs
@@ -69,33 +69,36 @@ impl DnsConfig {
             }
         }
 
-        let nsswitch_src = std::fs::File::open("/etc/nsswitch.conf")
-            .with_context(|| "Failed to open nsswitch.conf: /etc/nsswitch.conf")?;
+        if std::path::Path::new("/etc/nsswitch.conf").exists() {
+            let nsswitch_src = std::fs::File::open("/etc/nsswitch.conf")
+                .with_context(|| "Failed to open nsswitch.conf: /etc/nsswitch.conf")?;
 
-        let mut nsswitch = std::fs::File::create(format!("/etc/netns/{}/nsswitch.conf", ns_name))
-            .with_context(|| {
-            format!(
-                "Failed to open nsswitch.conf: /etc/netns/{}/nsswitch.conf",
-                ns_name
-            )
-        })?;
+            let mut nsswitch =
+                std::fs::File::create(format!("/etc/netns/{}/nsswitch.conf", ns_name))
+                    .with_context(|| {
+                        format!(
+                            "Failed to open nsswitch.conf: /etc/netns/{}/nsswitch.conf",
+                            ns_name
+                        )
+                    })?;
 
-        for line in std::io::BufReader::new(nsswitch_src).lines() {
-            writeln!(
-                nsswitch,
-                "{}",
-                Regex::new(r"^hosts:.*$")
-                    .unwrap()
-                    .replace(&line?, |_caps: &Captures| {
-                        "hosts: files mymachines myhostname dns"
-                    })
-            )
-            .with_context(|| {
-                format!(
-                    "Failed to overwrite nsswitch.conf: /etc/netns/{}/nsswitch.conf",
-                    ns_name
+            for line in std::io::BufReader::new(nsswitch_src).lines() {
+                writeln!(
+                    nsswitch,
+                    "{}",
+                    Regex::new(r"^hosts:.*$")
+                        .unwrap()
+                        .replace(&line?, |_caps: &Captures| {
+                            "hosts: files mymachines myhostname dns"
+                        })
                 )
-            })?;
+                .with_context(|| {
+                    format!(
+                        "Failed to overwrite nsswitch.conf: /etc/netns/{}/nsswitch.conf",
+                        ns_name
+                    )
+                })?;
+            }
         }
 
         Ok(Self { ns_name })
