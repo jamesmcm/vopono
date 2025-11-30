bca0aa7bbd7cb15b42a12eb07fdb36e8337be67a
Schwarzen <iianimosityii@gmail.com>
2025-11-30 01:27:57 -0600

    back to threadrng

--- vopono_core/src/util/wireguard.rs
+++ vopono_core/src/util/wireguard.rs
@@ -5,7 +5,6 @@ use base64::{
 
 use serde::Deserialize;
 use std::fmt::Display;
-use rand::rngs::OsRng;
 use x25519_dalek::{PublicKey, StaticSecret};
 
 const B64_ENGINE: GeneralPurpose = general_purpose::STANDARD;
@@ -43,8 +42,7 @@ impl Display for WgPeer {
 
 pub fn generate_keypair() -> anyhow::Result<WgKey> {
     // Generate new keypair
-    let mut rng = OsRng;
-    let private = StaticSecret::random_from_rng(&mut rng);
+    let private = StaticSecret::random_from_rng(&mut rand::rng());
     let public = PublicKey::from(&private);
     let public_key = B64_ENGINE.encode(public.as_bytes());
     let private_key = B64_ENGINE.encode(private.to_bytes());
